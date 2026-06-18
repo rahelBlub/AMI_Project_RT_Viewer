6426e8cd650105d352045c2dda5376e22016783a
Rahel <rahel.grandi@web.de>
2026-06-18 13:56:42 +0200

    added whole file RT

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.dicom_handler import DicomHandler
 # $python3 main.py --p ./data/RT/LungData_01
 
 if __name__ == "__main__":
-    data_path = "./data/RT/LungData_01"
+    data_path = "./data/RT/LUNG1-001/09-18-2008-StudyID-NA-69331/0.000000-NA-82046"
 
     d_handler = DicomHandler(data_path)
 
